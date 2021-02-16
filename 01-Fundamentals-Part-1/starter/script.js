09186d176907742ce81da28464c10057a9f23def
Sara <sahara.tijol@gmail.com>
2021-02-16 00:07:05 -0600

    Project started

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,9 @@
+// Assignment 1: Values and Variables
+
+const country = "USA";
+const continent = "North America";
+let population = 579;
+
+console.log(country);
+console.log(continent);
+console.log(population);
