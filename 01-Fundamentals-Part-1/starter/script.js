6395e6315b778518297e22ff45c3b163dca8e390
Sara <sahara.tijol@gmail.com>
2021-02-17 23:43:46 -0600

    Finished assignment

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,9 +1,31 @@
-// Assignment 1: Values and Variables
-
+// Assignment : Values and Variables
 const country = "USA";
 const continent = "North America";
 let population = 579;
-
 console.log(country);
 console.log(continent);
 console.log(population);
+
+// Data types
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+// let, const, var
+language = 'English';
+
+// Basic operators
+let halfPop = population / 2;
+console.log(halfPop);
+halfPop++;
+console.log(halfPop > 6);
+console.log(halfPop < 33);
+
+let description = country + ' is in ' + continent + ' ,and its ' + population + ' million people speak ' + language;
+
+console.log(description);
+
+
