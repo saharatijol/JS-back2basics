b97386b12ad2607108683aaaa71ac554da8abbde
Sara <sahara.tijol@gmail.com>
2021-02-20 23:22:24 -0600

    Equality

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -36,3 +36,17 @@ if (population > 33) {
   } else { console.log(
   `${country}'s population is ${33 - population} million below average`,
   ); }
+
+
+  const numNeighbours = prompt('How many neighbour countries does your country have?', );
+    
+  // LATER : This helps us prevent bugs
+    const numNeighbours = Number(prompt('How many neighbour countries does your country have?'), );
+    
+    if (numNeighbours === 1) { 
+      console.log('Only 1 border!');
+    } else if (numNeighbours > 1) { 
+      console.log('More than 1 border');
+    } else {
+      console.log('No borders');
+    }
