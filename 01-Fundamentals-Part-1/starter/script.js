8c91e75b5c4edae48ff97613bcc09afd6b0b15e2
Sara <sahara.tijol@gmail.com>
2021-02-21 20:21:04 -0600

    Refactor

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -33,9 +33,9 @@ console.log(description);
 
 if (population > 33) {
   console.log(`${country}'s population is above average`);
-  } else { console.log(
-  `${country}'s population is ${33 - population} million below average`,
-  ); }
+  } else { 
+    console.log(`${country}'s population is ${33 - population} million below average`,); 
+  }
 
 
   const numNeighbours = prompt('How many neighbour countries does your country have?', );
