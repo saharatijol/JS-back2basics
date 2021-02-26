ae5d9a01dcf1b51292c3f4fc2c931aeae69678f3
Sara <sahara.tijol@gmail.com>
2021-02-26 16:19:56 -0600

    Completed code challenge 1

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -9,11 +9,38 @@ BMI = mass / height ** 2 OR mass / (height * height).
 2. Calculate both their BMIs using the formula (you can even implement both versions)
 3. Create a boolean variable 'markHigherBMI' containing information about whether Mark has a higher BMI than John.
 
-TEST DATA 1: Marks weights 78kg and is 1.69m tall
+TEST DATA 1: Mark weighs 78kg and is 1.69m tall
             John weighs 92kg and is 1.95m tall.
 TEST DATA 2: Mark weighs 95kg and is 1.88m tall.
             John weighs 85kg and is 1.76m tall.
 */
 
+// TEST DATA 1
+let markWeight = 78;
+let markHeight = 1.69;
 
+let markBMI = markWeight / (markHeight ** 2);
+console.log("Mark's BMI: " + markBMI);
 
+let johnWeight = 92;
+let johnHeight = 1.95;
+
+let johnBMI = johnWeight / (johnHeight ** 2);
+console.log("John's BMI: " + johnBMI); 
+
+let isMarkHigher = Boolean(markBMI > johnBMI);
+console.log(isMarkHigher);
+
+// TEST DATA 2
+markWeight = 95;
+markHeight = 1.69;
+markBMI = markWeight / (markHeight ** 2);
+console.log(`New Mark's BMI: ${markBMI}`);
+
+johnWeight = 85;
+johnHeight = 1.76;
+johnBMI = johnWeight / (johnHeight ** 2);
+console.log(`New John's BMI: ${johnBMI}`);
+
+isMarkHigher = Boolean(markBMI > johnBMI);
+console.log(`New isMarkHigher: ${isMarkHigher}`);
